d3e47590821ef72b4c73e10d97263ef07dde06b8
Majid Vaghari <majid_vaghari2009@yahoo.com>
2024-06-22 08:10:19 +0330

    Add Model Fields to Django Admin

--- wallet/transactions/admin.py
+++ wallet/transactions/admin.py
@@ -6,8 +6,13 @@ from .models import Transaction, Wallet
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'sender', 'receiver', 'amount', 'status')
+    fields = ('uuid', 'sender', 'receiver', 'amount', 'scheduled_time', 'status', 'error_message', 'created', 'updated')
 
+    readonly_fields = ['uuid', 'status', 'error_message', 'created', 'updated']
 
 @admin.register(Wallet)
 class WalletAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'balance')
+    fields = ('uuid', 'balance', 'created', 'updated')
+
+    readonly_fields = ['uuid', 'balance', 'created', 'updated']
